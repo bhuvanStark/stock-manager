8a60266f4afd561cad3fee1c3512fe1352261345
bhuvanStark <bhuvaneshsundar2006@gmail.com>
2025-07-15 22:45:59 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA-Jc3Lwrvd06aXdzg9xhyp6YMaefaryIs",
+    authDomain: "stock-manager-24e42.firebaseapp.com",
+    projectId: "stock-manager-24e42",
+    storageBucket: "stock-manager-24e42.firebasestorage.app",
+    messagingSenderId: "116764560364",
+    appId: "1:116764560364:web:0daab1daf060923626cec9"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
