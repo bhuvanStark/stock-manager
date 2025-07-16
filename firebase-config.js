8d27549bece1ee5a9845b15ccdf0876a80313b10
bhuvanStark <bhuvaneshsundar2006@gmail.com>
2025-07-16 14:59:49 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,16 @@
 // firebase-config.js
 
-// Load Firebase v8 compatible
 const firebaseConfig = {
   apiKey: "AIzaSyA-Jc3Lwrvd06aXdzg9xhyp6YMaefaryIs",
   authDomain: "stock-manager-24e42.firebaseapp.com",
-  databaseURL: "https://stock-manager-24e42-default-rtdb.firebaseio.com",
+  databaseURL: "https://stock-manager-24e42-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "stock-manager-24e42",
   storageBucket: "stock-manager-24e42.appspot.com",
   messagingSenderId: "116764560364",
   appId: "1:116764560364:web:0daab1daf060923626cec9"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-const auth = firebase.auth();
+
 
