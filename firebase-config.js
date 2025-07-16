3bccad388ff4da0f545a1af974dc31875e46af2f
bhuvanStark <bhuvaneshsundar2006@gmail.com>
2025-07-16 14:42:25 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,17 @@
+// firebase-config.js
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Load Firebase v8 compatible
+const firebaseConfig = {
+  apiKey: "AIzaSyA-Jc3Lwrvd06aXdzg9xhyp6YMaefaryIs",
+  authDomain: "stock-manager-24e42.firebaseapp.com",
+  databaseURL: "https://stock-manager-24e42-default-rtdb.firebaseio.com",
+  projectId: "stock-manager-24e42",
+  storageBucket: "stock-manager-24e42.appspot.com",
+  messagingSenderId: "116764560364",
+  appId: "1:116764560364:web:0daab1daf060923626cec9"
+};
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyA-Jc3Lwrvd06aXdzg9xhyp6YMaefaryIs",
-    authDomain: "stock-manager-24e42.firebaseapp.com",
-    projectId: "stock-manager-24e42",
-    storageBucket: "stock-manager-24e42.firebasestorage.app",
-    messagingSenderId: "116764560364",
-    appId: "1:116764560364:web:0daab1daf060923626cec9"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const auth = firebase.auth();
 
