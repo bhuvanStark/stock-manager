f9688ccc0189d33655f602330d7b0b57cc73414a
bhuvanStark <bhuvaneshsundar2006@gmail.com>
2025-07-17 17:00:47 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,12 @@
 // firebase-config.js
-
-// Your Firebase config
-var firebaseConfig = {
-  apiKey: "AIzaSyA-Jc3Lwrvd06aXdzg9xhyp6YMaefaryIs",
-  authDomain: "stock-manager-24e42.firebaseapp.com",
-  databaseURL: "https://stock-manager-24e42-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "stock-manager-24e42",
-  storageBucket: "stock-manager-24e42.appspot.com",
-  messagingSenderId: "116764560364",
-  appId: "1:116764560364:web:0daab1daf060923626cec9"
-};
-
-// ✅ Safe init to prevent "already exists" error
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp({
+    apiKey: "AIzaSyA-Jc3Lwrvd06aXdzg9xhyp6YMaefaryIs",
+    authDomain: "stock-manager-24e42.firebaseapp.com",
+    databaseURL: "https://stock-manager-24e42-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "stock-manager-24e42",
+    storageBucket: "stock-manager-24e42.appspot.com",
+    messagingSenderId: "116764560364",
+    appId: "1:116764560364:web:0daab1daf060923626cec9"
+  });
 }
-
-
-
-
-
-
